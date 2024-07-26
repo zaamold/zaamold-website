a75acbe0043ceddfdf0f327c587cbf152cba09d1
Zachary Aamold <zma0198@gmail.com>
2024-07-25 21:52:43 -0500

    Removed major layout components from game screen (will remove entirely when styling homepage)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,20 @@
+"use client";
 import Image from "next/image";
-import type { ReactNode } from "react";
+import { type ReactNode } from "react";
 import { StoreProvider } from "./StoreProvider";
 import { Nav } from "./components/Nav";
 
 import "./styles/globals.css";
 import styles from "./styles/layout.module.css";
+import { usePathname } from "next/navigation";
 
 interface Props {
   readonly children: ReactNode;
 }
 
 export default function RootLayout({ children }: Props) {
+  const pathname = usePathname();
+
   return (
     <StoreProvider>
       <html lang="en">
@@ -18,65 +22,69 @@ export default function RootLayout({ children }: Props) {
           <section className={styles.container}>
             <Nav />
 
-            <header className={styles.header}>
-              <Image
-                src="/logo.svg"
-                className={styles.logo}
-                alt="logo"
-                width={100}
-                height={100}
-              />
-            </header>
+            {pathname != "/game" &&
+              (<header className={styles.header}>
+                <Image
+                  src="/logo.svg"
+                  className={styles.logo}
+                  alt="logo"
+                  width={100}
+                  height={100}
+                />
+              </header>)
+            }
 
             <main className={styles.main}>{children}</main>
 
-            <footer className={styles.footer}>
-              <span>Learn </span>
-              <a
-                className={styles.link}
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                React
-              </a>
-              <span>, </span>
-              <a
-                className={styles.link}
-                href="https://redux.js.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Redux
-              </a>
-              <span>, </span>
-              <a
-                className={styles.link}
-                href="https://redux-toolkit.js.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Redux Toolkit
-              </a>
-              <span>, </span>
-              <a
-                className={styles.link}
-                href="https://react-redux.js.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                React Redux
-              </a>
-              ,<span> and </span>
-              <a
-                className={styles.link}
-                href="https://reselect.js.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Reselect
-              </a>
-            </footer>
+            {pathname != "/game" &&
+              (<footer className={styles.footer}>
+                <span>Learn </span>
+                <a
+                  className={styles.link}
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  React
+                </a>
+                <span>, </span>
+                <a
+                  className={styles.link}
+                  href="https://redux.js.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Redux
+                </a>
+                <span>, </span>
+                <a
+                  className={styles.link}
+                  href="https://redux-toolkit.js.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Redux Toolkit
+                </a>
+                <span>, </span>
+                <a
+                  className={styles.link}
+                  href="https://react-redux.js.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  React Redux
+                </a>
+                ,<span> and </span>
+                <a
+                  className={styles.link}
+                  href="https://reselect.js.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Reselect
+                </a>
+              </footer>)
+            }
           </section>
         </body>
       </html>
