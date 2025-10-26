8cc4b25d0bdab5f58cb6e2af9c49863726c490a3
Zachary Aamold <zma0198@gmail.com>
2025-10-26 16:02:54 -0500

    Added animation to headshot outline, refactored to its own component

---
+++ components/ui/headshot.js
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import "./animations.css";
+
+export default function Headshot() {
+    return (
+        <div className="flex justify-center items-center relative w-64 h-64">
+            {/* Main image */}
+            <Image
+            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-30 w-64 h-64 object-cover rounded-full"
+            src="/images/headshot/headshot-01.png"
+            width={2002}
+            height={2002}
+            alt="Zach Aamold Image"
+            />
+
+            {/* First outline (slightly larger) */}
+            <Image
+            className="absolute px-1 py-4 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 scale-[1.15] w-64 h-64 object-cover rounded-full animate-rotate-slow"
+            src="/images/headshot/headshot-02-askew.png"
+            width={2150}
+            height={2175}
+            alt="Headshot Outline 1"
+            />
+
+            {/* Second outline (even larger) */}
+            <Image
+            className="absolute px-1 py-4 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 scale-[1.2] w-64 h-64 object-cover rounded-full animate-rotate-slow-reverse"
+            src="/images/headshot/headshot-03.png"
+            width={2250}
+            height={2250}
+            alt="Headshot Outline 2"
+            />
+        </div>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,36 +1,11 @@
-import Image from "next/image";
+import Headshot from "../../components/ui/headshot";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen bg-gray-100">
+    <div className="min-h-screen bg-gray-100">
       <p>Hello, world!</p>
-      <div className="flex justify-center items-center m-10 relative w-64 h-64">
-        {/* Main image */}
-        <Image
-          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-30 w-64 h-64 object-cover rounded-full"
-          src="/images/headshot/headshot-01.png"
-          width={2002}
-          height={2002}
-          alt="Zach Aamold Image"
-        />
-
-        {/* First outline (slightly larger) */}
-        <Image
-          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 scale-[1.08] w-64 h-64 object-cover rounded-full"
-          src="/images/headshot/headshot-02.png"
-          width={2150}
-          height={2150}
-          alt="Headshot Outline 1"
-        />
-
-        {/* Second outline (even larger) */}
-        <Image
-          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 scale-[1.15] w-64 h-64 object-cover rounded-full"
-          src="/images/headshot/headshot-03.png"
-          width={2250}
-          height={2250}
-          alt="Headshot Outline 2"
-        />
+      <div className="py-30 flex w-full justify-center items-center">
+      <Headshot />
       </div>
     </div>
   );
