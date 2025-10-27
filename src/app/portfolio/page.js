502f7d03787d48ea5d6e2405d34318a8651b693f
Zachary Aamold <zma0198@gmail.com>
2025-10-27 15:47:51 -0500

    Improved timeline

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -4,10 +4,10 @@ export default function PortfolioPage() {
   const careerEvents = [
     {
       title: "Web & Mobile Applications Developer",
-      subtitle: "Car Keys Express",
-      date: "Summer 2020 - Present",
+      subtitle: "Car Keys Express (Remote)",
+      date: "2020 - Present",
       description:
-        "Full-stack developer building mobile apps and websites. Supports Ecommerce and internal business management for automotive key replacement solutions.",
+        "Full-stack developer building mobile apps and websites. Supports Ecommerce and internal initiatives for automotive key replacement solutions.",
       technologies: [
         "React Native",
         "Expo",
@@ -26,22 +26,38 @@ export default function PortfolioPage() {
         "Jira",
         "GitLab",
         "GitHub"
+      ],
+      links: [
+        {
+          name: "Web Store",
+          href: "https://store.carkeysexpress.com",
+        },
+        {
+          name: "Company Site",
+          href: "https://carkeysexpress.com/",
+        },
       ]
     },
     {
       title: "Java Developer Contractor",
-      subtitle: "HYTE Technologies",
+      subtitle: "HYTE Technologies (Sherman, TX)",
       date: "Summers of 2018 & 2019",
       description:
         "Summer internships focused on large-scale refactors of code and creating JUnit test suites. Assisted in the evolution of software that is actively being used by Fortune 50 customers.",
       technologies: [
         "Java",
         "GitHub"
+      ],
+      links: [
+        {
+          name: "Company Site",
+          href: "https://hyte.io/",
+        },
       ]
     },
     {
       title: "Bachelor's in Computer Science & Communications",
-      subtitle: "Austin College, Sherman, TX",
+      subtitle: "Austin College (Sherman, TX)",
       date: "2016 – 2020",
       description:
         "Graduated summa cum laude. Learned CS core fundamentals, data structures, networking, software engineering, video game development, and several programming languages. Tutored CS students for two years. President of the Computer Science & Robotics Club for one year.",
@@ -56,6 +72,12 @@ export default function PortfolioPage() {
         "SQLite",
         "C",
         "Haskell",
+      ],
+      links: [
+        {
+          name: "School Site",
+          href: "https://www.austincollege.edu/",
+        },
       ]
     },
   ];
