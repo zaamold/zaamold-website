40effa97e2184e7d0795522ec69dcbd5e2f7d83a
Zachary Aamold <zma0198@gmail.com>
2025-10-27 16:32:12 -0500

    Added and implemented TechGrid component

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -1,3 +1,4 @@
+import TechGrid from "@/components/ui/tech-grid";
 import Timeline from "@/components/ui/timeline";
 
 export default function PortfolioPage() {
@@ -84,9 +85,18 @@ export default function PortfolioPage() {
 
   return (
     <main className="min-h-screen">
-      <section className="px-6 py-20">
+      <section className="px-6 pt-20 pb-10">
         <h2 className="text-4xl font-bold text-center mb-6">My Journey</h2>
         <Timeline events={careerEvents} />
+      </section>
+      <section className="px-6 pt-20 pb-10 bg-purple-50">
+        <h2 className="text-4xl font-bold text-center mb-6">My Website</h2>
+        <div className="max-w-4xl flex flex-col mx-auto gap-y-4">
+          <p className="text-xl">I built this website using many of the technologies and techniques I've picked up during my career as a software developer. Consider it a part of my portfolio.</p>
+          <p className="text-xl">I aim to always make my UI scale well for both desktop and mobile screens. You can test each page at different resolutions to see what I mean.</p>
+        </div>
+        <TechGrid />
+        <p className="text-center text-xl">Want to check my work? View the website's code on <a href="https://github.com/zaamold/zaamold-website" target="_blank" className="underline hover:text-purple-700 transition-all">GitHub</a>.</p>
       </section>
     </main>
   );
